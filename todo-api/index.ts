0f336eae6e63b0f4cd719b65b83b9f9d8289ed5a
pesekt1 <pesekt@gmail.com>
2025-09-28 17:02:02 +0200

    added SIGINT handler to gracefully exit

--- todo-api/index.ts
+++ todo-api/index.ts
@@ -1,9 +1,9 @@
+import cors from "cors";
+import "dotenv/config";
 import express from "express";
+import todoRouter from "./routers/todoRouter";
 import dbConnect from "./startup/dbConnect";
 import seed from "./startup/seed";
-import "dotenv/config";
-import todoRouter from "./routers/todoRouter";
-import cors from "cors";
 
 dbConnect();
 seed();
@@ -17,6 +17,23 @@ app.get("/", (req, res) => {
   res.send("Hello World!!!!");
 });
 
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
   console.log("Server is running on port 5000");
+});
+
+// ✅ Graceful shutdown for Render / Heroku / Docker
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received. Shutting down gracefully...");
+  server.close(() => {
+    console.log("HTTP server closed.");
+    process.exit(0);
+  });
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received. Exiting...");
+  server.close(() => {
+    console.log("HTTP server closed.");
+    process.exit(0);
+  });
 });
