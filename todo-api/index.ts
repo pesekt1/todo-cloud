c1eae665a351fd47a16789cfbb8e14e122e2ac2a
pesekt1 <pesekt@gmail.com>
2025-09-29 19:48:02 +0200

    test change

--- todo-api/index.ts
+++ todo-api/index.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use("/todos", todoRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!!!");
+  res.send("Hello World NEW");
 });
 
 app.listen(5000, () => {
